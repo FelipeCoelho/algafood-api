cac8ae27921f890b8b6c1709fde9a1a4f43575cc
Felipe Coelho <felipeisrael@gmail.com>
2020-03-06 10:16:33 -0300

    2,12

--- src/main/java/com/algaworks/algafood/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/notificacao/NotificadorEmail.java
@@ -5,11 +5,16 @@ import org.springframework.stereotype.Component;
 import com.algaworks.algafood.modelo.Cliente;
 
 @Component
-public class NotificadorEmail {
-	
-	public void notificar(Cliente cliente,String mensagem) {
-		System.out.printf("Notificando %s através do e-mail %s: %s \n", 
-				cliente.getNome(), cliente.getEmail(),mensagem);
+public class NotificadorEmail implements Notificador {
+
+	public NotificadorEmail() {
+		System.out.println("NotificadorEmail");
+	}
+
+	@Override
+	public void notificar(Cliente cliente, String mensagem) {
+		System.out.printf("Notificando %s através do e-mail %s: %s \n", cliente.getNome(), cliente.getEmail(),
+				mensagem);
 	}
 
 }
