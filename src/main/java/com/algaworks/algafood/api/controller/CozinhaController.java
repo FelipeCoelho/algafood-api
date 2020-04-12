21916350e5ec1c535332f04f5201ea452c11e2b2
Felipe Coelho <felipeisrael@gmail.com>
2020-04-12 20:29:17 -0300

    4.20

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,8 +3,10 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -34,10 +36,17 @@ public class CozinhaController {
 		return new CozinhasXmlWrapper(cozinhaRepository.listar());
 	}
 	
-	@ResponseStatus(HttpStatus.CREATED)
 	@GetMapping("/{cozinhaId}")
-	public Cozinha buscar(@PathVariable("cozinhaId") Long id) {
-		return cozinhaRepository.buscar(id);
+	public ResponseEntity<Cozinha> buscar(@PathVariable("cozinhaId") Long id) {
+		Cozinha cozinha = cozinhaRepository.buscar(id);
+		
+//		return ResponseEntity.status(HttpStatus.OK).body(cozinha);
+//		return ResponseEntity.ok(cozinha);
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.add(HttpHeaders.LOCATION, "http://api.algafood.local:8080/cozinhas");
+		
+		return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
 	}
 
 }
