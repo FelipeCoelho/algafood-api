4354b22407031a3721e9ea8af75be5dc30d9a974
Felipe Coelho <felipeisrael@gmail.com>
2020-04-12 21:05:33 -0300

    4.23

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -10,6 +10,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,8 +45,13 @@ public class CozinhaController {
 		if(cozinha != null) {
 			return ResponseEntity.ok(cozinha);
 		}
-		
-//		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Cozinha> adicionar(@RequestBody Cozinha cozinha){
+		Cozinha cozinhaSalva = cozinhaRepository.salvar(cozinha);
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(cozinhaSalva);
 	}
 }
