a4dd187f3ca68efd65ede10b1d09ad5e5586db1b
Felipe Coelho <felipeisrael@gmail.com>
2020-05-03 15:21:32 -0300

    5.4

--- src/main/java/com/algaworks/algafood/domain/repository/CidadeRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/CidadeRepository.java
@@ -2,8 +2,11 @@ package com.algaworks.algafood.domain.repository;
 
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+
 import com.algaworks.algafood.domain.model.Cidade;
 
+@Component
 public interface CidadeRepository {
 
 	List<Cidade> listar();
--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -2,8 +2,11 @@ package com.algaworks.algafood.domain.repository;
 
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+
 import com.algaworks.algafood.domain.model.Restaurante;
 
+@Component
 public interface RestauranteRepository {
 
 	List<Restaurante> listar();
