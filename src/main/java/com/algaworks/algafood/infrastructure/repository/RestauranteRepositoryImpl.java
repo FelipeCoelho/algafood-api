1868a05e6b6fe0cf450dac5594b19b7510f3ce32
Felipe Coelho <felipeisrael@gmail.com>
2020-05-03 21:52:21 -0300

    5.14

--- src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -8,6 +8,8 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import org.springframework.stereotype.Repository;
 
@@ -26,8 +28,14 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 		CriteriaBuilder builder = manager.getCriteriaBuilder();
 		
 		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
+		Root<Restaurante> root = criteria.from(Restaurante.class);
 		
-		criteria.from(Restaurante.class);
+		Predicate nomePredicate = builder.like(root.get("nome"), "%"+nome+"%");
+		Predicate taxaFreteInicialPredicate = builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial);
+		Predicate taxaFreteFinalPredicate = builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal);
+		
+		
+		criteria.where(nomePredicate, taxaFreteInicialPredicate, taxaFreteFinalPredicate);
 		
 		TypedQuery<Restaurante> query = manager.createQuery(criteria);
 		
