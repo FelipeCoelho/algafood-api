77afed1d3e894feb800cfe4caa889e19bc254419
Felipe Coelho <felipeisrael@gmail.com>
2020-05-06 20:42:38 -0300

    6.5

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,10 +1,6 @@
 insert into cozinha (id, nome) values (1, 'Tailandeza');
 insert into cozinha (id, nome) values (2, 'Indiana'); 
 
-insert into restaurante (nome, taxa_frete, cozinha_id) values('Thais Gourmet',10, 1);
-insert into restaurante (nome, taxa_frete, cozinha_id) values('Thais Delivery', 9.50, 1);
-insert into restaurante (nome, taxa_frete, cozinha_id) values('Tuk Tuk comida Indiana',15, 2); 
-
 insert into estado (id, nome) values (1,'São Paulo');
 insert into estado (id, nome) values (2, 'Parana');
 
@@ -13,6 +9,10 @@ insert into cidade (id, nome, estado_id) values(2,'São Paulo',1);
 insert into cidade (id, nome, estado_id) values(3,'Ribeirão Preto',1);
 insert into cidade (id, nome, estado_id) values(4,'Curitiba',2);
 insert into cidade (id, nome, estado_id) values(5,'Cascavel',2);
+
+insert into restaurante (nome, taxa_frete, cozinha_id, endereco_cidade_id, endereco_cep, endereco_logradouro, endereco_numero, endereco_bairro) values('Thais Gourmet',10, 1, 1, '14806122', 'Rua Teste','1000','Centro');
+insert into restaurante (nome, taxa_frete, cozinha_id) values('Thais Delivery', 9.50, 1);
+insert into restaurante (nome, taxa_frete, cozinha_id) values('Tuk Tuk comida Indiana',15, 2); 
 
 insert into permissao (id, nome, descricao) values (1,'Administrador','Administrador do sistema');
 
