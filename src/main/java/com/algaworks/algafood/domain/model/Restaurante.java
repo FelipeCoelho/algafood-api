6775bc5b2b58f637a26948c054b7947aebe7c074
Felipe Coelho <felipeisrael@gmail.com>
2020-08-11 23:02:28 -0300

    9.6.

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -17,8 +17,10 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PositiveOrZero;
 
 import org.hibernate.annotations.CreationTimestamp;
@@ -51,7 +53,9 @@ public class Restaurante {
 	private BigDecimal taxaFrete;
 
 //	@JsonIgnoreProperties("hibernateLazyInitializer")
-	@JsonIgnore
+//	@JsonIgnore
+	@Valid
+	@NotNull
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "cozinha_id", nullable = false)
 	private Cozinha cozinha;
